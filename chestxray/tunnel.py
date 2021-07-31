73b5571a3546a8ecccee4b95fcf766418d3a2c7b
SystematicError <systematicerror@users.noreply.github.com>
2021-07-31 17:52:00 +0530

    Add error checking

--- chestxray/tunnel.py
+++ chestxray/tunnel.py
@@ -13,7 +13,12 @@ def start_tunnel(host: str = "0.0.0.0", port: int = 13520) -> str:
     Returns:
         str: Session ID. Used by client to connect to server.
     """
-    url = ngrok.connect(port, "http").public_url
-    print(f"[tunnel] Created tunnel at {url}")
-    server_id = url.replace("http://", "").replace(".ngrok.io", "")
-    return server_id
+    try:
+        url = ngrok.connect(port, "http").public_url
+        print(f"[tunnel] Created tunnel at {url}")
+        server_id = url.replace("http://", "").replace(".ngrok.io", "")
+    except Exception:
+        print("[tunnel] Failed to create tunnel")
+        server_id = "unavailable"
+    finally:
+        return server_id
