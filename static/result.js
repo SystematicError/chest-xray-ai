39a5000ca738f919806f710f689bb75cdb5b121c
SystematicError <systematicerror@users.noreply.github.com>
2021-08-22 04:55:04 +0530

    Add image preview for results

--- static/result.js
+++ static/result.js
@@ -1,10 +1,10 @@
-function fetch_data () {
-    fetch(
-        "/api/scan-xray",
-        {method: "POST",
-         body: JSON.stringify(
-         {"image": localStorage["xray"]}
-         )})
-    .then(response => response.text())
-    .then(text => document.getElementById("data").innerHTML = text)
-}
+document.getElementsByClassName("img-fluid")[0].src = localStorage["xray"]
+
+fetch(
+    "/api/scan-xray",
+    {method: "POST",
+    body: JSON.stringify(
+        {"image": localStorage["xray"]}
+)})
+.then(response => response.text())
+.then(text => document.getElementById("data").innerHTML = text)
